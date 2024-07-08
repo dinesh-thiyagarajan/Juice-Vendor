4215839e178b5908905fca8f672b05e204e02a80
Dinesh <dineshkarthie@gmail.com>
2024-07-08 23:39:38 +0530

    Added manual refresh option to fetch the latest juice orders

--- composeApp/src/commonMain/kotlin/JuiceVendorApp.kt
+++ composeApp/src/commonMain/kotlin/JuiceVendorApp.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import composables.AddNewJuiceComposable
 import composables.JuiceListComposable
 import juicevendor.composeapp.generated.resources.Res
+import juicevendor.composeapp.generated.resources.ic_refresh
 import juicevendor.composeapp.generated.resources.juice_preparation
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import viewModels.JuiceVendorViewModel
@@ -24,6 +27,7 @@ import viewModels.JuiceVendorViewModel
 @Preview
 fun JuiceVendorApp(juiceVendorViewModel: JuiceVendorViewModel) {
     val showAddJuiceComposable = juiceVendorViewModel.showAddJuiceComposable.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
     MaterialTheme {
         Column {
             if (showAddJuiceComposable.value) {
@@ -31,8 +35,18 @@ fun JuiceVendorApp(juiceVendorViewModel: JuiceVendorViewModel) {
             } else {
                 Row(
                     modifier = Modifier.padding(20.dp).fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
+                    Image(
+                        painter = painterResource(Res.drawable.ic_refresh),
+                        contentDescription = "refresh juice orders",
+                        modifier = Modifier.size(30.dp).clickable {
+                            coroutineScope.launch {
+                                juiceVendorViewModel.getDrinkOrders()
+                            }
+                        },
+                        contentScale = ContentScale.Fit
+                    )
                     Image(
                         painter = painterResource(Res.drawable.juice_preparation),
                         contentDescription = "update juices list",
