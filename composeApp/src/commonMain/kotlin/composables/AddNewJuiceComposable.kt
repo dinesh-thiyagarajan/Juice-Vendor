8bb960f9d6f38b8534d2956d3babc818f476987f
Dinesh <dineshkarthie@gmail.com>
2024-07-09 08:53:48 +0530

    Close button added for add new composable

--- composeApp/src/commonMain/kotlin/composables/AddNewJuiceComposable.kt
+++ composeApp/src/commonMain/kotlin/composables/AddNewJuiceComposable.kt
@@ -1,5 +1,7 @@
 package composables
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -27,11 +30,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import data.Drink
+import juicevendor.composeapp.generated.resources.Res
+import juicevendor.composeapp.generated.resources.ic_close
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.painterResource
 import viewModels.JuiceVendorViewModel
 import java.util.UUID
 
@@ -44,7 +51,7 @@ fun AddNewJuiceComposable(juiceVendorViewModel: JuiceVendorViewModel) {
 
     var juiceName by remember { mutableStateOf("") }
     var juiceImage by remember { mutableStateOf("") }
-    var juiceAvailability by remember { mutableStateOf(false) }
+    var juiceAvailability by remember { mutableStateOf(true) }
     val scrollState = rememberScrollState()
 
     Column(
@@ -54,6 +61,19 @@ fun AddNewJuiceComposable(juiceVendorViewModel: JuiceVendorViewModel) {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Row (horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(Res.drawable.ic_close),
+                contentDescription = null,
+                modifier = Modifier.size(30.dp).clickable {
+                    juiceVendorViewModel.updateAddJuiceComposableVisibility(status = false)
+                },
+                contentScale = ContentScale.Fit
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
         OutlinedTextField(
             value = juiceName,
             isError = juiceName.isEmpty(),
@@ -88,16 +108,7 @@ fun AddNewJuiceComposable(juiceVendorViewModel: JuiceVendorViewModel) {
                 uncheckedThumbColor = MaterialTheme.colors.onSurface
             )
         )
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
-            Button(onClick = {
-                coroutineScope.launch {
-                    bottomSheetState.hide()
-                    juiceVendorViewModel.updateAddJuiceComposableVisibility(status = false)
-                }
-            }, enabled = true, modifier = Modifier.fillMaxWidth(0.3f)) {
-                Text("Close")
-            }
-
+        Row(horizontalArrangement = Arrangement.Center) {
             Button(onClick = {
                 coroutineScope.launch {
                     bottomSheetState.hide()
